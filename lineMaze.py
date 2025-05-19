417b8828c54144af0599c76096c4d5ffeb462435
Julian <113358009+skywqlkered@users.noreply.github.com>
2025-05-19 16:15:33 +0200

    feat: added linefollower from spike to copelia (not working)

--- lineMaze.py
+++ lineMaze.py
@@ -1,7 +1,7 @@
 from robots import *
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
-from linefollower.PID_controller import PID
-
+import time
+import asyncio
 
 client = RemoteAPIClient()
 sim = client.require("sim")
@@ -11,63 +11,82 @@ left_motor = Motor(sim, DeviceNames.MOTOR_LEFT_LINE, Direction.CLOCKWISE)
 right_motor = Motor(sim, DeviceNames.MOTOR_RIGHT_LINE, Direction.CLOCKWISE)
 color_sensor = ImageSensor(sim, DeviceNames.IMAGE_SENSOR_LINE)
 
-def is_red_detected(color_sensor):
-    """
-    Calculates the relative intensity of the red channel compared to
-    other channels
-    """
-    red_ratio_threshold = 1.5
-    red, green, blue = color_sensor.rgb()
-    print(red, green, blue)
-    red_intensity = red / (green + blue)
+class pid():
+    def __init__(self, kp, ki, kd, setpoint, deque_len):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
 
-    return red_intensity > red_ratio_threshold
+        self.deque_len = deque_len
+        self.previous_errors = [(time.time_ns()/1000000, 0)]
+
+    def compute_output(self, current_value):
+        error = current_value - self.setpoint
+        self.previous_errors.append((time.time_ns()/1000000,error))            
+        if len(self.previous_errors) > self.deque_len:
+            self.previous_errors.pop(0) 
+
+        dt = (self.previous_errors[-1][0] - self.previous_errors[-2][0])
+        derivative = (self.previous_errors[-1][1] - self.previous_errors[-2][1]) / dt
+        
+        integral = 0
+        first_time = self.previous_errors[0][0]
+        for datapoint_index in range(1,len(self.previous_errors)):
+            integral += (self.previous_errors[datapoint_index][0]-first_time) * self.previous_errors[datapoint_index][1]
+            first_time = self.previous_errors[datapoint_index][0]
+
+        P_term = error * self.kp
+        I_term = integral * self.ki
+        D_term = derivative * self.kd
+
+        return P_term + I_term + D_term
+
+# def change_motor_PID():
+#     color_sensor._update_image() # Updates the internal image
+#     reflection = color_sensor.reflection() # Gets the reflection from the image
+#     print(f"reflection: {reflection}")
+#     motorspeed: float = pid.compute(reflection, dt=1.0)
+#     basespeed: float = 5
+
+#     print(f"speed: {motorspeed}")
+#     left_motor.run(speed=basespeed+motorspeed)
+#     right_motor.run(speed=basespeed-motorspeed)
+#     print("------------------------------------")
+# def follow_line():
+#     """
+#     A very simple line follower that should be improved.
+#     """
+#     change_motor_PID()
+
+def main():
+    global global_reflection
+    kp: float = 1.5
+    ki: float = 0
+    kd: float = 0
+    setpoint = 48
+    
+    sim.startSimulation()
+
+    pid_controller = pid(kp, ki, kd, setpoint, 100)
+    # motor_pair.pair(motor_pair.PAIR_1, port.E, port.B)
+
+    while True:
+        color_sensor._update_image() # Updates the internal image
+        reflection = color_sensor.reflection() # Gets the reflection from the image
+        correcting_speed = pid_controller.compute_output(reflection)
+        print(reflection)
+        print(correcting_speed, "\n")
 
 
-def is_blue_detected(color_sensor):
-    """
-    Calculates the relative intensity of the blue channel compared to
-    other channels
-    """
-    blue_ratio_threshold = 1.5
-    red, green, blue = color_sensor.rgb()
-    blue_intensity = blue / (red + green)
+        basespeed = 15
+        left_motor.run(speed=basespeed-correcting_speed)
+        right_motor.run(speed=basespeed+correcting_speed)
 
-    return blue_intensity > blue_ratio_threshold
-
-def change_motor_PID():
-    color_sensor._update_image() # Updates the internal image
-    reflection = color_sensor.reflection() # Gets the reflection from the image
-    print(f"reflection: {reflection}")
-    motorspeed: float = pid.compute(reflection, dt=1.0)
-    basespeed: float = 5
-
-    print(f"speed: {motorspeed}")
-    left_motor.run(speed=basespeed+motorspeed)
-    right_motor.run(speed=basespeed-motorspeed)
-    print("------------------------------------")
-def follow_line():
-    """
-    A very simple line follower that should be improved.
-    """
-    change_motor_PID()
-
-
-
-# Starts coppeliasim simulation if not done already
-setpoint: float = 60.0
-"""backup
-kp: float = 0.05
-ki: float = 0.001
-kd: float = 0.0005
-"""
-kp: float = 0.14 
-ki: float = 0.00001
-kd: float = 0.001 # 0.0001 works
-pid = PID(Kp=kp, Ki=ki, Kd=kd, setpoint=setpoint)
-sim.startSimulation()
+        # if reflection == 0.0:
+        #     exit(404)
 
 
 # MAIN CONTROL LOOP
-while True:
-	follow_line()
+
+main()
